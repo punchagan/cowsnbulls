ff9fc73412b0c798873be74f9d5928c42d9a89e4
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-06-14 19:49:59 +0530

    Use fingerprinting on word_list.txt to get around caching issues.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+COMMIT_SHA=`git log --oneline -- word_list.txt|head -n 1|cut -f 1 -d " "`
+WORD_LIST="word_list_${COMMIT_SHA}.txt"
+
+echo "Changing word_list.txt to ${WORD_LIST}"
+sed -i -e "s/word_list.txt/${WORD_LIST}/g" cows_n_bulls.elm
 elm-make cows_n_bulls.elm --output=output/index.html
-cp word_list.txt output/
+git checkout -- cows_n_bulls.elm
+
+rm output/word_list*.txt
+cp word_list.txt "output/${WORD_LIST}"
+
 echo "cowsnbulls.in" > output/CNAME
+
 ghp-import -p output/

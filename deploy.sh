ffeadbe83c321b854ca97456b99aeefcd5a7a178
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-06-22 22:38:57 +0530

    Update deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ WORD_LIST="word_list_${COMMIT_SHA}.txt"
 
 echo "Changing word_list.txt to ${WORD_LIST}"
 sed -i -e "s/word_list.txt/${WORD_LIST}/g" cows_n_bulls.elm
-elm-make cows_n_bulls.elm --output=output/index.html
+elm-make cows_n_bulls.elm --output=output/elm.js
+cp index.html output/
 git checkout -- cows_n_bulls.elm
 
-rm output/word_list*.txt
+rm -f output/word_list*.txt
 cp word_list.txt "output/${WORD_LIST}"
 
 echo "cowsnbulls.in" > output/CNAME
